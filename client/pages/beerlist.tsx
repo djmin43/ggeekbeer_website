2950480c30fdd37bfb7fd0f32241b78bf7b8abb9
jay <dj.min43@gmail.com>
2021-04-12 01:18:39 +0900

    need to edit typescript

--- client/pages/beerlist.tsx
+++ client/pages/beerlist.tsx
@@ -6,20 +6,19 @@ import styles from '../styles/BeerList.module.css';
 import Button from '@material-ui/core/Button';
 import { SeasonalContext } from '../comps/contexts/SeasonalContext';
 import { RegularContext } from '../comps/contexts/RegularContext';
-
-
+import 'rxjs/add/operator/map';
 
 const Beer = () => {
 
-    const regular = useContext(RegularContext);
-    const seasonal = useContext(SeasonalContext);
+    const regular: any = useContext(RegularContext);
+    const seasonal: any = useContext(SeasonalContext);
 
     return (
         <div className={styles.beer} id="beer">
         {/* Regular Line */}
         <h1 >Year-round</h1>
         <Grid container spacing={5}>
-        {regular.map(item => <Grid  item xs={12} sm={6} md={4} key={item.id}>
+        {regular.map((item: any) => <Grid  item xs={12} sm={6} md={4} key={item.id}>
                     <Card className={styles.card} elevation={4}>
                     <h2>{item.name}</h2>
                     <h4>{item.descriptionShort}</h4>
@@ -32,7 +31,7 @@ const Beer = () => {
         {/* Seasonals */}
         <h1>Seasonals</h1>
             <Grid container spacing={5}>
-            {seasonal.map(item => <Grid  item xs={12} sm={6} md={4} key={item.id}>
+            {seasonal.map((item: any) => <Grid  item xs={12} sm={6} md={4} key={item.id}>
                     <Card className={styles.card} elevation={4}>
                     <h2>{item.name}</h2>
                     <h4>{item.descriptionShort}</h4>
