8e9f825583d78a14668aa910f496719887df92f8
jay <dj.min43@gmail.com>
2021-05-06 21:05:37 +0900

    navbar rework

--- client/comps/Navbar.tsx
+++ client/comps/Navbar.tsx
@@ -1,18 +1,27 @@
 import React from 'react'
 import styles from '../styles/Navbar.module.css'
 import Link from 'next/link'
+import Grid from '@material-ui/core/Grid'
 
 const Navbar = () => {
     return (
-        <>
-        <nav className={styles.navbar} >
-            <Link href="/"><a>Home</a></Link>
-            <Link href="/aboutus"><a>About Us</a></Link>
-            <Link href="/beers"><a>Our beer</a></Link>
-            <Link href="/news"><a>News</a></Link>
-            <Link href="/contact_us"><a>Contact Us</a></Link>
-        </nav>
-        </>
+        
+        <Grid className={styles.container} container>
+            <Grid xs={12} md={6}  item>
+                <nav className={styles.navbar} >
+                <Link href="/"><a>HOME</a></Link>
+                <Link href="/aboutus"><a>ABOUT</a></Link>
+                <Link href="/beers"><a>BEERS</a></Link>
+                <Link href="/"><a>LOCATION</a></Link>
+                <Link href="/"><a>BLOG</a></Link>
+                <Link href="/"><a>JOBS</a></Link>
+                <Link href="/news"><a>NEWS</a></Link>
+                <Link href="/contact_us"><a>CONTACT</a></Link>
+                </nav>
+            </Grid>
+        </Grid>
+
+        
     )
 }
 
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -15,8 +15,8 @@ export default function Home() {
     </Head>
       <Intro />
       <About />
-      <NewsList />
-      <BeerList />
+      {/* <NewsList />
+      <BeerList /> */}
       <ContactUs />
     </>
   );
