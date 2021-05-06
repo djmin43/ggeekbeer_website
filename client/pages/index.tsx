b3bbc3891eea0adb443c34c2c1fe3fba036094d2
jay <dj.min43@gmail.com>
2021-05-06 20:54:58 +0900

    intro image rendered.

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -3,6 +3,7 @@ import About from './about';
 import NewsList from './news_list';
 import BeerList from './beer_list';
 import ContactUs from './contact_us';
+import Intro from './intro'
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <meta name="title" content="끽비어컴퍼니"></meta>
       <meta name="description" content="크래프트맥주 양조장 끽비어컴퍼니입니다. 을지로 탭하우스에서 만나보실 수 있습니다."></meta>
     </Head>
+      <Intro />
       <About />
       <NewsList />
       <BeerList />
---
+++ client/pages/intro.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from '../styles/Intro.module.css'
+
+
+
+const intro = () => {
+    return (
+        <div className={styles.main}>
+            
+        </div>
+    )
+}
+
+export default intro
