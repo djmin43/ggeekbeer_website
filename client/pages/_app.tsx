c3c5a59fedec2d0f170c357e4d1429e07233baca
jay <dj.min43@gmail.com>
2021-05-07 21:27:39 +0900

    testing enviornmental variable for naver api key

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -6,8 +6,7 @@ import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
 
-  const NAVER = process.env.NAVER_KEY
-  console.log(NAVER)
+  
 
   return (
     <>
@@ -15,7 +14,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <link rel="icon" href="favicon.ico" />
       <meta name="title" content="끽비어컴퍼니"></meta>
       <meta name="description" content="한국 크래프트맥주 양조장 끽비어컴퍼니입니다. 을지로 탭하우스에서 만나보실 수 있습니다."></meta>
-      <script type="text/javascript" src="https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=%NAVER%"></script>
+      <script type="text/javascript" src="https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=process.env.NAVER_KEY"></script>
     </Head>
 
       <Layout>
