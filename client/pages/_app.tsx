7fd473d19b156ca8b4519e178df5805bbcbeb247
jay <dj.min43@gmail.com>
2021-05-07 21:37:58 +0900

    rework on script tag

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -14,7 +14,9 @@ function MyApp({ Component, pageProps }: AppProps) {
       <link rel="icon" href="favicon.ico" />
       <meta name="title" content="끽비어컴퍼니"></meta>
       <meta name="description" content="한국 크래프트맥주 양조장 끽비어컴퍼니입니다. 을지로 탭하우스에서 만나보실 수 있습니다."></meta>
-      <script type="text/javascript" src="https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=%NAVER_API%"></script>
+      <script type="text/javascript" >
+        script.src = "https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId={process.env.NAVER_API}"
+      </script>
     </Head>
 
       <Layout>
