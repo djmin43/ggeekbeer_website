688ef11b26349b4998270cbdeb09b7b8cdd4d5f4
jay <dj.min43@gmail.com>
2021-05-10 12:15:52 +0900

    fix on naver api key

--- client/pages/location.tsx
+++ client/pages/location.tsx
@@ -27,12 +27,10 @@ const location = () => {
     return (
         <>
         <Head>
+        <script type="text/javascript" src="https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=ll2fjdhq8m"></script>
         </Head>
         <div id="location">
-
-
             끽비어 탭하우스는 을지로에 있습니다.
-
         <div id="map" className={styles.maps}></div>
 
       </div>
