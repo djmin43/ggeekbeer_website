9a4a3e374e42dba91ea31277b975511c9edda500
jay <dj.min43@gmail.com>
2021-05-10 12:39:22 +0900

    naver map fix marker location

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -22,6 +22,7 @@ export default function Home({regularBeers, seasonalBeers}: any) {
       <title>끽비어입니다.</title>
       <meta name="title" content="끽비어컴퍼니"></meta>
       <meta name="description" content="한국 크래프트맥주 양조장 끽비어컴퍼니입니다. 을지로 탭하우스에서 만나보실 수 있습니다."></meta>
+
     </Head>
          <Intro />
           <About />
--- client/pages/location.tsx
+++ client/pages/location.tsx
@@ -5,17 +5,43 @@ import styles from '../styles/Location.module.css'
 
 const location = () => {
 
-    // var map = null;
+    var map = null;
 
     const initMap = () => {
+
         var map = new naver.maps.Map('map', {
-            center: new naver.maps.LatLng(37.5669239, 126.9934278),
-            zoom: 10
+            center: new naver.maps.LatLng(37.3595316, 127.1052133),
+            zoom: 15,
+            zoomControl: true
         });
         var marker = new naver.maps.Marker({
-            position: new naver.maps.LatLng(37.5669239, 126.9934278),
+            position: new naver.maps.LatLng(37.3595316, 127.1052133),
             map: map
         });
+        var contentString = [
+            '   <h3>끽비어을지로펍</h3>',
+            '   <p>서울특별시 중구 태평로1가 31 | 서울특별시 중구 세종대로 110 서울특별시청<br />',
+
+            '       02-120 | 공공,사회기관 &gt; 특별,광역시청<br />',
+            '       <a href="http://www.seoul.go.kr" target="_blank">www.seoul.go.kr/</a>',
+            '   </p>',
+            '</div>'
+        ].join('');
+
+        var infowindow = new naver.maps.InfoWindow({
+            content: contentString
+        });
+        
+        naver.maps.Event.addListener(marker, "click", function(e) {
+            if (infowindow.getMap()) {
+                infowindow.close();
+            } else {
+                infowindow.open(map, marker);
+            }
+        });
+        
+        infowindow.open(map, marker);
+        
     }
     
 
@@ -28,10 +54,13 @@ const location = () => {
         <>
         <Head>
         <script type="text/javascript" src="https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=ll2fjdhq8m"></script>
+
         </Head>
         <div id="location">
             끽비어 탭하우스는 을지로에 있습니다.
         <div id="map" className={styles.maps}></div>
+
+
 
       </div>
       </>
