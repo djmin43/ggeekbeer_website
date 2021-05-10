dacb9a1010affc943083bf679d9f23594fb26336
jay <dj.min43@gmail.com>
2021-05-10 16:28:20 +0900

    request from client updated.#1

--- client/comps/Navbar.tsx
+++ client/comps/Navbar.tsx
@@ -9,15 +9,20 @@ const Navbar = () => {
         <>
 
             <Grid className={styles.container} container>
+            
+            <Grid xs={12} md={6}  item>
+                
+            </Grid>
+
             <Grid xs={12} md={6}  item>
                 <nav className={styles.navbar} >
                 <Link href="/"><a>HOME</a></Link>
                 <Link href="/#about"><a>ABOUT</a></Link>
                 <Link href="/#beers"><a>BEERS</a></Link>
                 <Link href="/#location"><a>LOCATION</a></Link>
-                <Link href="/"><a>BLOG</a></Link>
-                <Link href="/"><a>JOBS</a></Link>
-                <Link href="/"><a>NEWS</a></Link>
+                {/* <Link href="/"><a>BLOG</a></Link> */}
+                {/* <Link href="/"><a>JOBS</a></Link> */}
+                {/* <Link href="/"><a>NEWS</a></Link> */}
                 <Link href="/#contact"><a>CONTACT</a></Link>
                 </nav>
             </Grid>
--- client/pages/beers/ggulggeok.tsx
+++ client/pages/beers/ggulggeok.tsx
@@ -12,9 +12,10 @@ const ggulggeok = () => {
                 <div className={styles.container} >
                     <h2>꿀꺽 Ggul Ggeok - Craft Lager</h2>
                     <h4><i></i></h4>
-                    <span><i>ABV: 4.6% • IBU 18 • 형태: Keg, Bottle(330ml)</i></span>
                     <p>'꿀꺽'은 크래프트 라거로 맥주의 많은 매력들 중 다가가기 쉽고 마시기 편한 장점을 가진맥주입니다. 여기에 '홉'의 개성을 부드럽게 살려주고자 노력했습니다. </p>
                     <p>편안하게 소비자에게 다가가며,메인 스트림과 다른 크래프트 맥주의 매력을 보여주고 싶었습니다. 크래프트 맥주를 좋아하는 사람들,처음 접하게 될 사람들 간의 경계를 허물고 모두 즐길 수 있는 '꿀꺽'이 되길 바랍니다.</p>
+                    <span><i>ABV: 4.6% • IBU 18 • 형태: Keg, Bottle(330ml)</i></span>
+
                 </div>
             </Grid>
         </Grid>
