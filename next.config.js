3ccb47bd672e84b8765a1eed39e191d3430ffe3b
jay <dj.min43@gmail.com>
2021-10-04 20:40:06 +0900

    comment out redirect

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: 'https://ggeekbeer.oopy.io/',
-        permanent: false,
-      },
-    ]
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/',
+  //       destination: 'https://ggeekbeer.oopy.io/',
+  //       permanent: false,
+  //     },
+  //   ]
+  // },
 }
