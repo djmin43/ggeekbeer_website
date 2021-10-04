ac19e9a545dd2ef110eff1b234fe72fd1ba21ca7
jay <dj.min43@gmail.com>
2021-10-04 20:11:53 +0900

    new og image

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ const Home: NextPage = () => {
         <meta property="og:description" content="맥주를 사랑하는 사람들이 모여 만든 공간입니다. 국내외 브루어리 맥주를 선별하여 제공합니다." key="description" />
       </Head>
       <Head>
-        <meta property="og:image" content="https://scontent.ficn3-1.fna.fbcdn.net/v/t39.30808-6/214705339_5818441701530905_2161549511797673616_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=a26aad&_nc_ohc=SaG8bND4kx4AX_YnDEm&_nc_ht=scontent.ficn3-1.fna&oh=114b56d760f56ba0fe80c56b42e6bec3&oe=616027E2" key="image" />
+        <meta property="og:image" content="https://scontent.ficn3-1.fna.fbcdn.net/v/t1.6435-9/123139391_4654108014630952_6374064862724962720_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=a26aad&_nc_ohc=8KG5dlcIRqQAX_mqYWE&tn=W-DCgKfCvncNO5OJ&_nc_ht=scontent.ficn3-1.fna&oh=e1d25c54f34ad0fd49096c951a873769&oe=6180E34F" key="image" />
       </Head>
       <Head>
         <meta property="og:url" content="https://ggeekbeer.com" key="url" />
